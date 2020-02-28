0b11989a55e5bfce5562b7805bf4f407b191d919
Nurbek <akhmedov@infosystems.uz>
2020-02-28 21:35:19 +0500

    fix

--- src/pages/statistics.svelte
+++ src/pages/statistics.svelte
@@ -1,11 +1,7 @@
-<Section {route}>
   <nu-card width="400px">
     <Pie />
   </nu-card>
-</Section>
 
 <script>
   import {Pie} from 'co/Chart'
-  import Section from 'co/base/Section.svelte'
-  export let route
 </script>
